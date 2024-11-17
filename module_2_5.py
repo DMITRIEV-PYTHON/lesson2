31dfe9d00c94db74bfc0de288b02a42d0281893c
DMITRIEV-PYTHON <dmitriev.python@gmail.com>
2024-11-17 21:00:40 +0300

    11

---
+++ module_2_5.py
@@ -0,0 +1,14 @@
+# Напишите функцию get_matrix с тремя параметрами n, m и value,
+# которая будет создавать матрицу(вложенный список) размерами n строк и m столбцов,
+# заполненную значениями value и возвращать эту матрицу в качестве результата работы.
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    print(matrix)
+    return matrix
+get_matrix(2 ,2, 10)
+get_matrix(3,5,42)
+get_matrix(4,2,13)
