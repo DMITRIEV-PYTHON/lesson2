858d41ef15c34d19722055fc40a17efd5c79299a
DMITRIEV-PYTHON <dmitriev.python@gmail.com>
2024-11-17 15:17:18 +0300

    11

---
+++ module_2_4.py
@@ -0,0 +1,29 @@
+#Дан список чисел numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+#Используя этот список составьте второй список primes содержащий только простые числа.
+#А так же третий список not_primes, содержащий все не простые числа.
+#Выведите списки primes и not_primes на экран(в консоль).
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+n = 0
+primes = []
+not_primes = []
+for i in range(len(numbers)):  # начинаем перебирать список
+    is_prime = True  # отметка простоты числа
+    k = 0
+    n = numbers[i]
+    if n < 2:  # исключить 1 из обоих списков
+        print(n, '- не простое и не сложное число')
+        continue
+    else:
+        for j in range(2, i):
+            if n % j == 0:
+                is_prime = False
+                break
+                #k = k + 1
+    if not is_prime:
+        primes.append(n)
+    else:
+        not_primes.append(n)
+is_prime = True
+print("primes", primes)
+print("not_primes", not_primes)
+
