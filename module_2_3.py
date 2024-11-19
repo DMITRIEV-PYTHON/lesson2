c1c87f0a8400f73aaf6d8cbcf63d9d91d3cba606
DMITRIEV-PYTHON <dmitriev.python@gmail.com>
2024-11-19 23:45:06 +0300

    доработка

--- module_2_3.py
+++ module_2_3.py
@@ -5,13 +5,9 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 a = len(my_list)
 i = 0
 while i < a:
-    if my_list[i] > 0:
+    if my_list[i] < 0:
+        print("отрицательное число", my_list[i])
+        break
+    elif my_list[i] > 0:
         print(my_list[i])
-        i = i + 1
-        continue
-    else:
-        if my_list[i] == 0:
-            i = i + 1
-        else:
-            if my_list[i] < 0:
-                break
+    i += 1
